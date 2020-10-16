a9557c09815685dd8513575edc93b1a9d41ddf25
Frederik Schönfeldt <fschoenfeldt@aidminutes.com>
2020-10-16 14:37:58 +0200

    wip notes

--- lib/live_view_studio_web/live/servers_live.ex
+++ lib/live_view_studio_web/live/servers_live.ex
@@ -2,6 +2,8 @@ defmodule LiveViewStudioWeb.ServersLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Servers
+  alias LiveViewStudio.Servers.Server
+  # alias LiveViewStudioWeb.Router
 
   def mount(_params, _session, socket) do
     servers = Servers.list_servers()
@@ -33,73 +35,11 @@ defmodule LiveViewStudioWeb.ServersLive do
     {:noreply, socket}
   end
 
-  def render(assigns) do
-    ~L"""
-    <h1>Servers</h1>
-    <div id="servers">
-      <div class="sidebar">
-        <nav>
-          <%= for server <- @servers do %>
-            <div>
-              <%= live_patch link_body(server),
-                    to: Routes.live_path(
-                              @socket,
-                              __MODULE__,
-                              id: server.id
-                        ),
-                    class: if server == @selected_server, do: "active" %>
-            </div>
-
-          <% end %>
-        </nav>
-      </div>
-      <div class="main">
-        <div class="wrapper">
-          <div class="card">
-            <div class="header">
-              <h2><%= @selected_server.name %></h2>
-              <span class="<%= @selected_server.status %>">
-                <%= @selected_server.status %>
-              </span>
-            </div>
-            <div class="body">
-              <div class="row">
-                <div class="deploys">
-                  <img src="/images/deploy.svg">
-                  <span>
-                    <%= @selected_server.deploy_count %> deploys
-                  </span>
-                </div>
-                <span>
-                  <%= @selected_server.size %> MB
-                </span>
-                <span>
-                  <%= @selected_server.framework %>
-                </span>
-              </div>
-              <h3>Git Repo</h3>
-              <div class="repo">
-                <%= @selected_server.git_repo %>
-              </div>
-              <h3>Last Commit</h3>
-              <div class="commit">
-                <%= @selected_server.last_commit_id %>
-              </div>
-              <blockquote>
-                <%= @selected_server.last_commit_message %>
-              </blockquote>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    """
-  end
-
   defp link_body(server) do
-    assigns = %{name: server.name}
+    assigns = %{name: server.name, status: server.status}
 
     ~L"""
+    <span class="status <%= @status %>"></span>
     <img src="/images/server.svg">
     <%= @name %>
     """
