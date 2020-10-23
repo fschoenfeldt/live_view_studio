785a68fbcb3ee9ec1896eed5907a3112393b55b4
Frederik Schönfeldt <fschoenfeldt@aidminutes.com>
2020-10-23 11:38:52 +0200

    finish notes

--- lib/live_view_studio_web/live/servers_live.ex
+++ lib/live_view_studio_web/live/servers_live.ex
@@ -78,6 +78,16 @@ defmodule LiveViewStudioWeb.ServersLive do
     end
   end
 
+  def handle_event("validate", %{"server" => params}, socket) do
+    changeset =
+      %Server{}
+      |> Servers.change_server(params)
+      |> Map.put(:action, :insert)
+
+    socket = assign(socket, changeset: changeset)
+    {:noreply, socket}
+  end
+
   defp link_body(server) do
     assigns = %{name: server.name, status: server.status}
 
